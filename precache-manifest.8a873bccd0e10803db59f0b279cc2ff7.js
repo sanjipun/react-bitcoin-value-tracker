b246e2b346bd13e23ee493dcc4239fa6c0189e5a
sanjinsky <iamsanjipun@gmail.com>
2020-12-18 13:40:48 +0545

    Updates

---
+++ precache-manifest.8a873bccd0e10803db59f0b279cc2ff7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5abfff266e38c4dfa4bd73ccadd8e0",
+    "url": "/react-bitcoin-value-tracker/index.html"
+  },
+  {
+    "revision": "410fcb295fd303263ac2",
+    "url": "/react-bitcoin-value-tracker/static/css/2.027e9bb2.chunk.css"
+  },
+  {
+    "revision": "d465596ac254c9ed9e28",
+    "url": "/react-bitcoin-value-tracker/static/css/main.07a718d4.chunk.css"
+  },
+  {
+    "revision": "410fcb295fd303263ac2",
+    "url": "/react-bitcoin-value-tracker/static/js/2.7c64d496.chunk.js"
+  },
+  {
+    "revision": "610359362e2f9d5fa420eb531608a3c9",
+    "url": "/react-bitcoin-value-tracker/static/js/2.7c64d496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d465596ac254c9ed9e28",
+    "url": "/react-bitcoin-value-tracker/static/js/main.563eaa35.chunk.js"
+  },
+  {
+    "revision": "b7445a826cbcf5005fad",
+    "url": "/react-bitcoin-value-tracker/static/js/runtime-main.7291ca61.js"
+  }
+]);
